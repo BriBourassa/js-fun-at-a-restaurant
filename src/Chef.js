3d79438819d35f17a7275b8d6cd8906a831413c0
Brianne Bourassa <shinybri1011@gmail.com>
2022-10-16 12:51:24 -0600

    Putz around more and get some cool answers

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,31 @@
+
+
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name,
+    this.restaurant = restaurant
   }
+
+
+greetCustomer(customer, isMorning){
+  
+  if (isMorning) {
+   return `Good morning, ${customer}!`
+} else {
+  // console.log(`Hello, ${customer}!`)
+  return `Hello, ${customer}!`
+}
+}
+checkForFood(foodItem) {
+  if (this.restaurant.menus.breakfast.includes(foodItem) || 
+  this.restaurant.menus.lunch.includes(foodItem) ||
+  this.restaurant.menus.dinner.includes(foodItem)){
+//need this. for CLASS
+// || checks all 3 menus
+    return `Yes, we're serving ${foodItem.name} today!`
+  } else 
+  return `Sorry, we aren't serving ${foodItem.name} today.`
+}
 }
 
 module.exports = Chef;
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,11 +1,50 @@
 
 
 function createRestaurant(restaurantName){
-
+  var restaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
 }
+// console.log(restaurant);
+return restaurant
+};
+
+
+function addMenuItem(restaurant, menuItem){
+  if (menuItem.type === 'breakfast' && !restaurant.menus.breakfast.includes(menuItem)) {
+    restaurant.menus.breakfast.push(menuItem)
+  } else if (menuItem.type === 'lunch' && !restaurant.menus.lunch.includes(menuItem)) {
+    restaurant.menus.lunch.push(menuItem)
+  } else if (menuItem.type === 'dinner' && !restaurant.menus.dinner.includes(menuItem)) {
+    restaurant.menus.dinner.push(menuItem)
+  }
+};
+// !flippin the boolean
+  
+
+
+function removeMenuItem(restaurant, menuItem, type){
+  for (var i = 0; i < menuItem.length; i++){
+    if (menuItem === restaurant.menus[type[i].name]){
+      restaurant.menuItem.splice([i], 1); 
+
+      return `No one is eating our ${menuItem} - it has been removed from the ${type} menu!`
+    } 
+     else {
+      return `Sorry, we don't sell ${menuItem}, try adding a new recipe!`
+    }
+  }
+};
+
+
+
 
 module.exports = {
   createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  addMenuItem,
+  removeMenuItem
 }
